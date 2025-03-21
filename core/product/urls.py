e399266279ab5d788bf367344f4d8f2433dd0107
16211994 <cakek433@gmail.com>
2025-03-21 23:04:54 +0300

    Register city url

--- core/product/urls.py
+++ core/product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, FavoriteViewSet, MyListingsViewSet
+from .views import ProductViewSet, CategoryViewSet, FavoriteViewSet, MyListingsViewSet, CityViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')  # Ensure correct name is given
 router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'cities', CityViewSet, basename='city')
 router.register(r'favorites', FavoriteViewSet, basename='favorite')
 router.register(r'my-listings', MyListingsViewSet, basename='my-listings')
 
