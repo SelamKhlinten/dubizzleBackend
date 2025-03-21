c143ca4f302566074bb80ef2e779d63ba28842d0
16211994 <cakek433@gmail.com>
2025-03-21 23:03:22 +0300

    Add city in admin side

--- core/product/admin.py
+++ core/product/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, City
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'icon', 'image']
 
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'region']  # Display city name and region in the admin panel
+    search_fields = ['name', 'region']  # Allow searching by name or region
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(City, CityAdmin)  # Register the City model
