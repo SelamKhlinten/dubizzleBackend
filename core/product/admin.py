307db7a23a2399262e7c13e66f90f3bde8712911
16211994 <cakek433@gmail.com>
2025-03-22 22:21:14 +0300

    Remove the registered product in the admin

--- core/product/admin.py
+++ core/product/admin.py
@@ -8,12 +8,12 @@ class CityAdmin(admin.ModelAdmin):
     list_display = ['name', 'region']  # Display city name and region in the admin panel
     search_fields = ['name', 'region']  # Allow searching by name or region
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'city', 'price', 'seller', 'created_at']
-    search_fields = ['title', 'category__name', 'city__name', 'seller__username']
-    list_filter = ['category', 'city', 'created_at']
-    ordering = ['-created_at']
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'category', 'city', 'price', 'seller', 'created_at']
+#     search_fields = ['title', 'category__name', 'city__name', 'seller__username']
+#     list_filter = ['category', 'city', 'created_at']
+#     ordering = ['-created_at']
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(City, CityAdmin)  # Register the City model
-admin.site.register(Product, ProductAdmin)  # Register the Product model
+# admin.site.register(Product, ProductAdmin)  # Register the Product model
