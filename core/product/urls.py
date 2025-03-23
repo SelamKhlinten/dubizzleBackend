9361e57614bb5bbb4d2ba7ff51eb5fc2b2170f91
16211994 <cakek433@gmail.com>
2025-03-23 18:49:24 +0300

    Just rewrite the code

--- core/product/urls.py
+++ core/product/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CategoryViewSet, FavoriteViewSet, MyListingsViewSet, CityViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='product')  # Ensure correct name is given
+router.register(r'', ProductViewSet, basename='product')  # Ensure correct name is given
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'cities', CityViewSet, basename='city')
 router.register(r'favorites', FavoriteViewSet, basename='favorite')
