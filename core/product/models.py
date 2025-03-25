4c005133bdb9122616f67d1892fc06c249226208
16211994 <cakek433@gmail.com>
2025-03-25 21:50:02 +0300

    Rewrite the favorite model

--- core/product/models.py
+++ core/product/models.py
@@ -93,7 +93,7 @@ class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='favorited_by')
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     class Meta:
         unique_together = ('user', 'product')  # Prevent duplicate favorites
 
